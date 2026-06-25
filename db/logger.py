4b6924b4ecea4fa8d76f21603eea480e22e90dbe
Sadinick <sadinik26@gmail.com>
2026-06-25 10:06:35 +0530

    Created SQLite database and weather table

--- db/logger.py
+++ db/logger.py
@@ -1,7 +1,23 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 DATABASE_URL = "sqlite:///anomalies.db"
 
 engine = create_engine(DATABASE_URL)
 
-print("Database initialized successfully")
+with engine.connect() as conn:
+
+    conn.execute(text("""
+        CREATE TABLE IF NOT EXISTS weather_data (
+
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+            timestamp DATETIME NOT NULL,
+
+            temperature REAL NOT NULL
+
+        );
+    """))
+
+    conn.commit()
+
+print("Database and weather_data table created successfully.")
